99a741d28e53aa1fdbb1f26faeb6a10ddc7738b5
TalionSD <101660980+TalionSD@users.noreply.github.com>
2025-08-09 22:13:47 +0330

    Implement Cyrus Playground

---
+++ app/[locale]/playground/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import type React from "react";
+
+export default function PlaygroundLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
